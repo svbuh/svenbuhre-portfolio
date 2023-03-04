54232fec3c796b11478ccf5577938b42d2fa3300
sven.buhre@bdr.de <buhresven@gmail.com>
2023-03-04 22:22:24 +0100

    changing sanity imports

--- sanity-backend/sanity.cli.ts
+++ sanity-backend/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'mxrsn5pp',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
